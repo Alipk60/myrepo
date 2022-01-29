45b76171af8f63487a9655feb3bf1726616ddf0b
ali <ali@yhoo.com>
2022-01-29 16:51:21 +0000

    commited by ali

---
+++ file1.java
@@ -0,0 +1,10 @@
+total 3248
+-rw-rw-r-- 1 pourtarkigmail pourtarkigmail       3 Jan 29 16:31 check_idle
+drwxr-xr-x 2 jenkins        jenkins           4096 Jan 29 15:33 hsperfdata_jenkins
+drwxr-xr-x 2 root           root              4096 Jan 29 15:33 hsperfdata_root
+drwx------ 2 jenkins        jenkins           4096 Jan 29 15:33 jetty-0_0_0_0-8080-war-_-any-4853282395456517987
+drwx------ 2 pourtarkigmail pourtarkigmail    4096 Jan 29 15:34 ssh-SkBQuwFfx3uh
+drwx------ 3 root           root              4096 Jan 29 15:33 systemd-private-a6ff6b7b6fc94c53a5d8a34c7ef0538b-rtkit-daemon.service-gK6MMk
+drwx------ 3 root           root              4096 Jan 29 15:33 systemd-private-a6ff6b7b6fc94c53a5d8a34c7ef0538b-systemd-timesyncd.service-K5mm4M
+-rw-r--r-- 1 jenkins        jenkins        3293397 Jan 29 15:33 winstone4870828006262101450.jar
+modified 
